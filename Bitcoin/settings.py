f1984a49ba37a0eceeaa824e15f56e92b4a387fd
Gourav Kumar Singh <gouravrajput257@gmail.com>
2022-09-28 15:05:28 -0500

    Chages for the deployment

--- Bitcoin/settings.py
+++ Bitcoin/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-866l@6llvqyk=7h$1q6_@&%z^@ho@jakx#mb7#0vrwohfvn%6!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["carefi-api-qrlt.onrender.com"]
+ALLOWED_HOSTS = ["carefi-api-qrlt.onrender.com", "127.0.0.1"]
 
 
 # Application definition
@@ -38,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'corsheaders',
     'BitcoinAPI',
 ]
 
@@ -60,7 +63,7 @@ ROOT_URLCONF = 'Bitcoin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,9 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
