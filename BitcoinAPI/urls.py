24ceb07a9732c7254d8eee72acb818605abba620
Gourav Kumar Singh <gouravrajput257@gmail.com>
2022-09-28 15:06:07 -0500

    Added the name for the home route

--- BitcoinAPI/urls.py
+++ BitcoinAPI/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('api_auth/', include('rest_framework.urls')),
     path(
         'all/', views.BitcoinViewSet.as_view({'get': 'list'}), name='allBitcoin'),
-    path('getCurrentPrice/<currency>/',
+    path('getCurrentPrice/<str:currency>/',
          views.getBitcoinPrice, name='currentPrice'),
-    path('getBitcoinListByTimestamp/<currency>/<from_timestamp>/<to_timestamp>',
-         views.getBitcoinPricesByTimestamp, name='bitcoinList')
+    path('getBitcoinListByTimestamp/<str:currency>/<int:from_timestamp>/<int:to_timestamp>',
+         views.getBitcoinPricesByTimestamp, name='bitcoinList'),
+    path('api-auth/', include('rest_auth.urls')),
 ]
